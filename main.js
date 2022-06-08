257e5bad35d5d67dbcf031df88778825754b38ad
David Song <songdasol@gmail.com>
2022-06-08 04:12:29 +0000

    created function to respond to arrow keys.

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 var $car = document.querySelector('.car');
 var $body = document.querySelector('body');
+
+function start(e) {
+  switch (e.keyCode) {
+    case 37:
+      movecar('left');
+      break;
+    case 38:
+      movecar('up');
+      break;
+    case 39:
+      movecar('right');
+      break;
+    case 40:
+      movecar('down');
+      break;
+  }
+}
